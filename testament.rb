a78e5b8413105fe1dddcf4c38dd7bcbe3f971c48
Emil Soman <emil@emil-virtual-machine.(none)>
2012-09-19 20:47:13 +0530

    Testament updated to use commandment

--- testament.rb
+++ testament.rb
@@ -1,26 +1,21 @@
+require_relative 'commandments/survival_commandment'
+
+#Any live cell with fewer than two live neighbours dies, as if caused by under-population
+
+#Any live cell with two or three live neighbours lives on to the next generation.
+
+#Any live cell with more than three live neighbours dies, as if by overcrowding.
+
+#Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.
+
 class Testament
+
+  def initialize
+    @commandments = [ SurvivalCommandment.new  ]
+  end
   
   def find_cells_passing_judgement(space)
-    apply_first_commandment(space)
-    apply_second_commandment(space)
-    apply_third_commandment(space)
-    apply_fourth_commandment(space)
   end
 
-  #Any live cell with fewer than two live neighbours dies, as if caused by under-population
-  def apply_first_commandment(space)
-  end
-
-  #Any live cell with two or three live neighbours lives on to the next generation.
-  def apply_second_commandment(space)
-  end
-
-  #Any live cell with more than three live neighbours dies, as if by overcrowding.
-  def apply_third_commandment(space)
-  end
-
-  #Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.
-  def apply_fourth_commandment(space)
-  end
 
 end
